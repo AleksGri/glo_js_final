258dca00fa1dd73a618c5f95c29c6060899c2418
AleksGri <aleks032118@gmail.com>
2021-10-23 12:31:20 -0700

    FEAT(script.js) add popUp

--- script/script.js
+++ script/script.js
@@ -23,9 +23,87 @@ window.addEventListener('DOMContentLoaded', function() {
 
   pageNavigation();
 
+  const togglePopup = () => {
 
+    const callbackBtn = document.querySelector('#top-callback'),
+          callBackModalWindow = document.querySelector('#callback'),
+          body = document.querySelector('body');
 
+         // popupForm = popupWindow.querySelector('#form3');
 
+    const popupHendler = function() {
+
+      const popAnimation = function (opacity, displayValue){
+        callBackModalWindow.style.opacity = `${opacity}%`;
+        function initiateTimeOut() {
+          setTimeout(function() { opacityChanger();}, 50);
+        }
+
+        function opacityChanger() {
+          if(displayValue === 'block') {
+            callBackModalWindow.style.display = displayValue;
+            opacity += 10;
+            callBackModalWindow.style.opacity = `${opacity}%`;
+            if (opacity < 100) {
+                initiateTimeOut(); 
+            }
+
+          } else {
+            opacity -= 10;
+            callBackModalWindow.style.opacity = `${opacity}%`;
+            if (opacity > 0) {
+                initiateTimeOut(); 
+            } else {
+              callBackModalWindow.style.display = displayValue;
+            }
+          } 
+        }
+        initiateTimeOut();
+      };
+
+      if(!callBackModalWindow.style.display || callBackModalWindow.style.display === 'none') {
+        if(window.innerWidth < 768) {
+          callBackModalWindow.style.display = 'block';  
+        } else {
+          popAnimation(0, 'block');
+        }
+      } else {
+        if(window.innerWidth < 768) {
+          callBackModalWindow.style.display = 'none';
+        } else {
+          popAnimation(100, 'none');
+        }
+      }
+    };
+
+    callbackBtn.addEventListener('click', ()=>{
+      popupHendler();
+      callBackModalWindow.addEventListener('click', (event) => {
+      let target = event.target;
+      target = target.closest('div');
+      //console.log(target);
+      
+
+      if (target.classList.contains('modal-close')) {
+        popupHendler();
+      } else {
+        // Тут будет обработчик формы
+      }
+      });
+
+      body.addEventListener('click', (event)=>{
+        console.log("tut");
+          let target = event.target;
+          target = target.closest('#callback');
+          console.log(target);
+          
+          if(!target) {popupHendler();}
+      });
+      
+    });
+  };
+
+  togglePopup();
 
 
 });
