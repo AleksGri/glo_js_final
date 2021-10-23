4a188911f8a0af7c0f6e3494a2c8d0a83f2c5e99
AleksGri <aleks032118@gmail.com>
2021-10-23 14:14:55 -0700

    FEAT(script.js) add scrollBack

--- script/script.js
+++ script/script.js
@@ -110,4 +110,50 @@ window.addEventListener('DOMContentLoaded', function() {
   togglePopup();
 
 
+  const scrollBack = () => {
+    const services = document.querySelector('#services'),
+          upButton = document.querySelector('.up'),
+          topSlider = document.querySelector('.top-slider');
+    let mouseDown;
+
+    window.addEventListener('scroll', ()=>{
+      const position = services.getBoundingClientRect();
+
+      if(position.y < 100) {
+        upButton.style.display = 'block';
+      } else {
+        upButton.style.display = 'none';
+      }
+    });
+
+    upButton.addEventListener('mousedown', (event)=>{
+
+      mouseDown = true;
+
+      function initiateTimeOut() {
+          setTimeout(function() { scroller();}, 50);
+        }
+
+        function scroller() {
+          window.scrollBy(0, -50);
+          let topSliderPosition = topSlider.getBoundingClientRect();
+          if (topSliderPosition.y < 0 && mouseDown) {
+                initiateTimeOut(); 
+            }
+        }
+        initiateTimeOut();
+    });
+
+    upButton.addEventListener('mouseup', (event)=>{
+      mouseDown = false; 
+    });
+  };
+
+  scrollBack();
+
+
+
+
+
+
 });
